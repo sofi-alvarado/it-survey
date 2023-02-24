7a7a84e3a253077dbd7ac08b304cb8214563bbd4
Sofía Alvarado <alvaradosofiaH@outlook.com>
2023-02-24 00:17:55 -0600

    Adding Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './Components/NavBar'
+import Home from './Components/Home';
 import GeneralPage from './Components/GeneralPage';
 
 function App() {
   return (
       <div className="App">
         <NavBar />
+        <Home />
         <GeneralPage />
     </div>
   );
