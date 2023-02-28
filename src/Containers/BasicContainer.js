402aea1499dddb1924b62892c0917fa5e06b17fe
Sofía Alvarado <alvaradosofiaH@outlook.com>
2023-02-28 13:40:47 -0600

    Adding containers and first Chart

--- src/Containers/BasicContainer.js
+++ src/Containers/BasicContainer.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import CardItem from '../Components/CardItem';
-
 import Container from 'react-bootstrap/Container';
+import BarChart from '../Components/BarChart';
+import myData from "../resultados_totales.json";
+import '../Components/styles/Charts.css'
+
+export const options = {
+    indexAxis: 'y',
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        position: 'right',
+      },
+      title: {
+        display: true,
+        text: '¿Cuál de los siguientes describe tu situación de empleo? ',
+      },
+    },
+  };
+  
+  const labels = Object.keys(myData.situacionDeEmpleo);
+  
+  export const data = {
+    labels,
+    datasets: [
+      {
+        data: labels.map((item) => myData.situacionDeEmpleo[item]),
+        backgroundColor: '#f8aa14',
+      }
+    ],
+  };
 
 const BasicContainer = () => {
     return (
@@ -15,6 +44,8 @@ const BasicContainer = () => {
                 <hr className="hr border border-white border-3" />
                 <div className='row d-flex justify-content-center align-items-center'>
                     <h2 className='primary-text'>Información Básica</h2>
+                        <p className='secundary-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est pellentesque elit ullamcorper dignissim cras tincidunt.</p>
+                        <BarChart options={options} data={data} />
                 </div>
             </Container>
 
