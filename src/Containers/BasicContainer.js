7cb576dcde76cca3d3276e2917fcc0ae686054c5
Sofía Alvarado <alvaradosofiaH@outlook.com>
2023-04-09 23:31:28 -0600

    Adding text in basic info section

--- src/Containers/BasicContainer.js
+++ src/Containers/BasicContainer.js
@@ -3,13 +3,11 @@ import CardItem from '../Components/CardItem';
 import BarChart from '../Components/BarChart';
 import DonutChart from '../Components/DonutChart';
 import myData from '../resultados_totales.json';
+import textData from '../text';
 import ChartComponent from '../Components/ChartComponent';
 
 const BasicContainer = () => {
     const text = {
-        title: 'Información Básica',
-        chartTitle: 'Chart Title',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est pellentesque elit ullamcorper dignissim cras tincidunt.',
         question1: '¿Cuál de los siguientes describe tu situación de empleo?',
         question2: '¿Cuál de las siguientes opciones te describe mejor?',
         question3: '¿Cómo describes tu modalidad de trabajo?'
@@ -20,13 +18,14 @@ const BasicContainer = () => {
             <CardItem
                 id='basic-info'
                 className='basic-info-card'
-                title={text.title}
+                title={textData.cards.basicInformation.title}
+                chartDescription={textData.cards.basicInformation.description}
             />
         {/* Question 1 */}
             <ChartComponent
-                title={text.title}
-                chartTitle={text.chartTitle}
-                chartDescription={text.text}
+                title={textData.cards.basicInformation.title}
+                chartTitle={textData.chartDescription.situacionDeEmpleo.title}
+                chartDescription={textData.chartDescription.situacionDeEmpleo.description}
                 chart={<BarChart
                     chartInfo={myData.situacionDeEmpleo}
                     autoSkipp={false} />}
@@ -34,9 +33,9 @@ const BasicContainer = () => {
             />
         {/* Question 2 */}
             <ChartComponent
-                title={text.title}
-                chartTitle={text.chartTitle}
-                chartDescription={text.text}
+                title={textData.cards.basicInformation.title}
+                chartTitle={textData.chartDescription.softwareDev.title}
+                chartDescription={textData.chartDescription.softwareDev.description}
                 chart={<BarChart
                     chartInfo={myData.softwareDev}
                     autoSkipp={false} />}
@@ -44,9 +43,9 @@ const BasicContainer = () => {
             />
         {/* Question 3 */}
             <ChartComponent
-                title={text.title}
-                chartTitle={text.chartTitle}
-                chartDescription={text.text}
+                title={textData.cards.basicInformation.title}
+                chartTitle={textData.chartDescription.modalidadDeTrabajo.title}
+                chartDescription={textData.chartDescription.modalidadDeTrabajo.description}
                 chart={<DonutChart
                     chartInfo={myData.modalidadDeTrabajo} />}
                 question={text.question3}
